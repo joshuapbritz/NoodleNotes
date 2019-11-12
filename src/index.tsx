fbdc32fb1e3069e4b3c4ecbd19aea31bec48d34d
Joshua Britz <joshua@onepointfour.io>
2019-11-12 16:29:53 +0200

    Fixes

--- src/index.tsx
+++ src/index.tsx
@@ -10,3 +10,38 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+function getTextNodes(element: HTMLElement): any[] {
+  let nodes: any[] = [];
+  if (!!element) {
+    for (const node of Array.from(element.childNodes)) {
+      if (node.nodeType === 3) {
+        nodes.push(node);
+      } else if (
+        node.nodeType === 1 ||
+        node.nodeType === 9 ||
+        node.nodeType === 11
+      ) {
+        nodes = [...nodes, ...getTextNodes(node as any)];
+      }
+    }
+  }
+
+  return nodes;
+}
+
+getTextNodes(window.document.body).forEach((node: Text) => {
+  const span = document.createElement('span');
+  span.className = `canhighlight`;
+
+  span.addEventListener('click', () => {
+    const mark = document.createElement('mark');
+    mark.className = `highlighted`;
+    mark.appendChild(node.cloneNode(true));
+    span.replaceWith(mark);
+  });
+
+  span.append(node.cloneNode(true));
+
+  node.replaceWith(span);
+});
